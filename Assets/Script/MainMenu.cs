56ccf46c82488787858b70e339d724828c808c9e
SMKdeveloper <simonemarino.smk@gmail.com>
2026-01-29 19:59:06 +0100

    Add initial project assets and main menu scene
    Added graphics, UI elements, and TextMesh Pro resources to the Assets folder. Created IntroScene and MainMenu scenes with associated metadata. Included a basic MainMenu.cs script. Updated EditorBuildSettings to include new scenes.

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Button startButton, soundButton, exitButton;
+    
+    [SerializeField] private string LevelToLoad;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        exitButton.onClick.AddListener(Application.Quit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
